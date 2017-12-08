f07beb7e60676271c112254ac45c6f5a16b162e0
xiaweizi <1012126908@qq.com>
2017-12-08 13:58:59 +0800

    初次提交

---
+++ app/src/main/java/com/xiaweizi/androidmd/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.xiaweizi.androidmd
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
