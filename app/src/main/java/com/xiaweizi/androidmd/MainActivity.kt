487294d2ce356a3009c6c3310be9eb6d8aa3a3df
xiaweizi <1012126908@qq.com>
2017-12-08 20:29:29 +0800

    引入相关需要的库

--- app/src/main/java/com/xiaweizi/androidmd/MainActivity.kt
+++ app/src/main/java/com/xiaweizi/androidmd/MainActivity.kt
@@ -2,8 +2,6 @@ package com.xiaweizi.androidmd
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
