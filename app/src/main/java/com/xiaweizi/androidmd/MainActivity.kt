b635fdd782150fef863f6d07dd97d7987754d927
xiaweizi <1012126908@qq.com>
2017-12-09 17:07:46 +0800

    添加侧滑交互

--- app/src/main/java/com/xiaweizi/androidmd/MainActivity.kt
+++ app/src/main/java/com/xiaweizi/androidmd/MainActivity.kt
@@ -1,25 +1,138 @@
 package com.xiaweizi.androidmd
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
+import android.support.v7.widget.Toolbar
+import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.listener.OnItemClickListener
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mIvHead: ImageView
+    private var myAdapter: MyAdapter? = null
+    private lateinit var mRecyclerView: RecyclerView
+    private lateinit var mRefreshLayout: SwipeRefreshLayout
+    private lateinit var mToolbar: Toolbar
+    private lateinit var mDrawerLayout: DrawerLayout
+
+    private var mToggle: ActionBarDrawerToggle? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+        init()
+        initListener()
+        mRefreshLayout.isRefreshing = true
+        getData(0)
+    }
+
+    private fun initView() {
+        mRecyclerView = findViewById(R.id.recyclerView)
+        mRefreshLayout = findViewById(R.id.refresh_layout)
+        mToolbar = findViewById(R.id.toolbar)
+        mDrawerLayout = findViewById(R.id.dl_main)
         val nav: NavigationView = findViewById(R.id.nv_left)
         mIvHead = nav.getHeaderView(0).findViewById(R.id.iv_head)
+    }
+
+    private fun init() {
+        myAdapter = MyAdapter()
+        myAdapter!!.openLoadAnimation(BaseQuickAdapter.SLIDEIN_LEFT)
+        mRecyclerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        mRecyclerView.adapter = myAdapter
+        mRefreshLayout.setColorSchemeColors(Color.RED, Color.BLUE, Color.GREEN)
+        setSupportActionBar(mToolbar)
         Glide.with(this).load(Contants.HEAD_IMAGE_URL)
                 .into(mIvHead)
-        val beginTransaction = supportFragmentManager.beginTransaction()
-        beginTransaction.replace(R.id.fl_content, MainFragment())
-        beginTransaction.commit()
+        //给打开左侧菜单的按钮是指特效
+        mToggle = ActionBarDrawerToggle(this@MainActivity,
+                mDrawerLayout,
+                mToolbar,
+                R.string.open, R.string.close)
+        mToggle!!.syncState()
+        mDrawerLayout.addDrawerListener(mToggle!!)
+
+    }
+
+    private fun initListener() {
+        mRefreshLayout.setOnRefreshListener {
+            mRefreshLayout.isRefreshing = true
+            getData(1)
+        }
+        mRecyclerView.addOnItemTouchListener(object : OnItemClickListener() {
+            override fun onSimpleItemClick(adapter: BaseQuickAdapter<*, *>?, view: View?, position: Int) {
+                if (adapter is MyAdapter) {
+                    Log.i("itemTouch", adapter.data[position].toString())
+                }
+            }
+        })
+        myAdapter!!.setOnLoadMoreListener {
+            getData(2)
+        }
+    }
+
+    /**
+     * 获取数据
+     * 0 第一次进入
+     * 1 下拉刷新
+     * 2 上啦加载更多
+     */
+    fun getData(state: Int) {
+        MyClient.getInstance()
+                .create(MyService::class.java, Contants.MASTER_URL)
+                .weaponInfo
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    var status = it.status
+                    if (status == 0) {
+                        when (state) {
+                            0 -> {
+                                myAdapter!!.setNewData(it.data)
+                                mRefreshLayout.isRefreshing = false
+                            }
+                            1 -> {
+                                Toast.makeText(this, "刷新成功！", Toast.LENGTH_SHORT).show()
+                                Collections.shuffle(it.data)
+                                myAdapter!!.setNewData(it.data)
+                                mRefreshLayout.isRefreshing = false
+                            }
+                            2 -> {
+                                Thread(Runnable {
+                                    Thread.sleep(800)
+                                    runOnUiThread {
+                                        myAdapter!!.addData(it.data)
+                                        myAdapter!!.loadMoreComplete()
+                                    }
+                                }).start()
+
+                            }
+                        }
+                    } else {
+                        mRefreshLayout.isRefreshing = false
+                        Toast.makeText(this, "获取数据失败！", Toast.LENGTH_SHORT).show()
+                    }
+                }, {
+                    mRefreshLayout.isRefreshing = false
+                    Toast.makeText(this, "获取数据失败！", Toast.LENGTH_SHORT).show()
+                })
     }
 
 }
